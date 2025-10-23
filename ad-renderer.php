bd04448bbe9a2d768310c7c5de209199245bbeda
Samyak Jain <samyak.jn2431@gmail.com>
2025-10-23 16:40:58 +0530

    gpt is added in adrenderer

--- ad-renderer.php
+++ ad-renderer.php
@@ -8,6 +8,17 @@ require_once __DIR__ . '/renderer/footer-renderer.php';
 
 
 if ( get_option('adx_enabled','false') === 'true' ) {
+    // echo "<script>console.log('adxbyms-gpt loading');</script>";
+    wp_register_script(
+        'adxbyms-gpt',
+        'https://securepubads.g.doubleclick.net/tag/js/gpt.js',
+        array(),
+        ADXMS_GPT_VERSION,           // let Google handle caching
+        true
+    );
+    wp_enqueue_script('adxbyms-gpt');
+    // echo "<script>console.log('adxbyms-gpt loaded');</script>";
+
     add_action('wp_head', 'adx_v4_render_header_ads');
     add_action('wp_footer', 'adx_v4_render_footer_ads');
 }
